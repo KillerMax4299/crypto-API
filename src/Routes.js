fd62b3785e9f94d9bf44ef6acc88a5ef05a22968
Killermax <thekillermax1999@gmail.com>
2023-09-21 19:55:59 +0530

    env file added

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const Axios = require("axios");
 const router = express.Router();
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
 router.get("/", function (req, res) {
   res.json({ status: "success", message: "APi Connected" });
 });
 
 router.get("/top10", async function (req, res) {
-  const { data } = await Axios.get("https://api.wazirx.com/api/v2/tickers");
+  const { data } = await Axios.get(process.env.API_URL);
   const jsonArray = Object.values(data);
   jsonArray.sort((a, b) => b.last - a.last);
   // const sortedValues = jsonArray
