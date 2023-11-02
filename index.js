adc633231306fcc87100b8777715fde10a95bd92
killermax <thekillermax1999@gmail.com>
2023-11-02 20:18:05 +0530

    vercel config added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const router = require("./Routes");
+const router = require("./src/Routes");
 app.use("", router);
 app.listen(
   3000,
